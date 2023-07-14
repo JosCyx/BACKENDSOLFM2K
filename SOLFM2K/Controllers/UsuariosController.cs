9ec1c07f0875db7bd922c2a33e905bdfba819e86
JosCyx <joseguillermojm.jm@gmail.com>
2023-07-14 10:34:19 -0500

    creacion del controlador usuarios

---
+++ SOLFM2K/Controllers/UsuariosController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SOLFM2K.Models;
+
+namespace SOLFM2K.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuariosController : ControllerBase
+    {
+        private readonly SolicitudContext _context;
+
+        public UsuariosController(SolicitudContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Usuarios
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Usuario>>> GetUsuarios()
+        {
+          if (_context.Usuarios == null)
+          {
+              return NotFound();
+          }
+            return await _context.Usuarios.ToListAsync();
+        }
+
+        // GET: api/Usuarios/5
+        [HttpGet("{UsId}")]
+        public async Task<ActionResult<Usuario>> GetUsuario(int UsId)
+        {
+          if (_context.Usuarios == null)
+          {
+              return NotFound();
+          }
+            var usuario = await _context.Usuarios.FindAsync(UsId);
+
+            if (usuario == null)
+            {
+                return NotFound();
+            }
+
+            return usuario;
+        }
+
+        // PUT: api/Usuarios/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{UsId}")]
+        public async Task<IActionResult> PutUsuario(int UsId, Usuario usuario)
+        {
+            if (UsId != usuario.UsId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(usuario).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!UsuarioExists(UsId))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Usuarios
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Usuario>> PostUsuario(Usuario usuario)
+        {
+          if (_context.Usuarios == null)
+          {
+              return Problem("Entity set 'SolicitudContext.Usuarios'  is null.");
+          }
+            _context.Usuarios.Add(usuario);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(PostUsuario), new { id = usuario.UsId }, usuario);
+        }
+
+        // DELETE: api/Usuarios/5
+        [HttpDelete("{UsId}")]
+        public async Task<IActionResult> DeleteUsuario(int UsId)
+        {
+            if (_context.Usuarios == null)
+            {
+                return NotFound();
+            }
+            var usuario = await _context.Usuarios.FindAsync(UsId);
+            if (usuario == null)
+            {
+                return NotFound();
+            }
+
+            _context.Usuarios.Remove(usuario);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool UsuarioExists(int UsId)
+        {
+            return (_context.Usuarios?.Any(e => e.UsId == UsId)).GetValueOrDefault();
+        }
+    }
+}
