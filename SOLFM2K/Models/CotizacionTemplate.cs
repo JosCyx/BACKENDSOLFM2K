4aa17b1cda9c4aa9df966898b41dda8e392fe27a
JosCyx <joseguillermojm.jm@gmail.com>
2023-08-18 09:33:42 -0500

    no templates

--- SOLFM2K/Models/CotizacionTemplate.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SOLFM2K.Models
-{
-    public class CotizacionTemplate
-    {
-        public CabSolCotizacion? Cabecera { get; set; }
-        public List<DetSolCotizacion>? Detalles { get; set; }
-        public List<ItemSector>? Items { get; set; }
-    }
-}
--- SOLFM2K/Models/OrdenComprasTemplate.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SOLFM2K.Models
-{
-    public class OrdenComprasTemplate
-    {
-        public DetSolCotizacion? Cabecera { get; set; }
-        public List<DetSolCotizacion>? Detalles { get; set; }
-        public List<ItemSector>? Items { get; set; }
-    }
-}
