04f67714466639f0458f4dc3912fb04e54358d8c
jgranados4 <jgranados@est.ups.edu.ec>
2023-12-05 15:23:02 -0500

    refactor: Cambios class OCAXTemplate

---
+++ SOLFM2K/Models/OCAXTemplate.cs
@@ -0,0 +1,14 @@
+﻿namespace SOLFM2K.Models
+{
+    public partial class OCAXTemplate
+    {
+        public string DetOrden { get; set; }
+        public string DetcodProducto { get; set; }
+        public string DetdesProducto { get; set; }
+        public Decimal Detcantidad { get; set; }
+        public Decimal Detprecio { get; set; }
+        public Decimal  DetTotal { get; set; }
+
+
+    }
+}
